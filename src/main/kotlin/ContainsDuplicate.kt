4015323ec9d85116f1217e678de1631440e92548
bruno-c-nogueira <brunocamargos2009@hotmail.com>
2022-11-21 20:20:30 -0300

    solving some challenges

---
+++ src/main/kotlin/ContainsDuplicate.kt
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/contains-duplicate/
+class ContainsDuplicate {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val map = HashMap<Int, Int>()
+        nums.forEach {
+            if (map.containsKey(it)){
+                return true
+            }else {
+                map[it] = 1
+            }
+        }
+        return false
+    }
+
+}
+
+//https://leetcode.com/problems/binary-tree-inorder-traversal/
+class TraversalTree {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        var list = mutableListOf<Int>()
+        return checkTree(root, list)
+    }
+
+    fun checkTree(root: TreeNode?, list: MutableList<Int>): MutableList<Int> {
+        if(root == null) return list
+        val list = checkTree(root.left, list)
+        list.add(root.`val`)
+
+        return checkTree(root.right, list)
+    }
+}
